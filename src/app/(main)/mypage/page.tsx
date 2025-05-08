ab85cc3c8dba8949bf769ec89b23a6964793ed98
suzykang <pc_suzi@daum.net>
2025-05-09 03:12:25 +0900

    feat: 마이페이지 API 연동

--- src/app/(main)/mypage/page.tsx
+++ src/app/(main)/mypage/page.tsx
@@ -2,12 +2,19 @@
 
 import { useRouter } from 'next/navigation';
 
-import { mockUserProfile, Profile } from '@/features/user';
+import { Profile, useUser } from '@/features/user';
 import { logout } from '@/features/user/api/logout';
 import { Header } from '@/shared/components';
 
 export default function MyPage() {
   const router = useRouter();
+  const { user } = useUser();
+
+  if (!user) {
+    return <div>로딩중...</div>;
+  }
+
+  const { username, profile_image, introduction } = user;
 
   const handleEdit = () => {
     router.push('/mypage/edit');
@@ -30,7 +37,11 @@ export default function MyPage() {
     <>
       <Header title="마이페이지" />
       <div className="mt-12 flex flex-col items-center px-4">
-        <Profile profile={mockUserProfile} />
+        <Profile
+          username={username}
+          profile_image={profile_image ?? ''}
+          introduction={introduction ?? ''}
+        />
         <button
           onClick={handleEdit}
           className="button-text mb-4 w-1/2 rounded-full bg-rose-100 px-4 py-2 text-gray-800 transition-colors hover:bg-rose-200"
