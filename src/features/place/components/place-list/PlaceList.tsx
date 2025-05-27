3ade57ced90286a28592e830694335e7f8aab406
suzykang <pc_suzi@daum.net>
2025-05-27 19:25:26 +0900

    style: placeList 높이 조절

--- src/features/place/components/place-list/PlaceList.tsx
+++ src/features/place/components/place-list/PlaceList.tsx
@@ -24,8 +24,8 @@ export function PlaceList({ places }: PlaceListProps) {
     >
       <Drawer.Portal>
         <Drawer.Content
-          className="fixed right-0 bottom-0 left-0 z-50 mx-auto flex h-full max-w-[430px] flex-col rounded-t-2xl border-t bg-white shadow-lg"
-          style={{ maxHeight: '100svh' }}
+          className="fixed right-0 bottom-0 left-0 z-10 mx-auto flex h-full max-w-[430px] flex-col rounded-t-2xl border-t bg-white shadow-lg"
+          style={{ maxHeight: 'calc(100svh - 60px)' }}
         >
           <div className="mx-auto mt-4 h-1.5 w-12 rounded-full bg-zinc-300" />
           <Drawer.Title className="hidden">추천 장소 목록</Drawer.Title>
