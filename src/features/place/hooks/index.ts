c0e63a22391d7bae8ea6000bb59647616ade4a28
suzykang <pc_suzi@daum.net>
2025-07-06 22:07:31 +0900

    feat: useScrollRestoration 폴더 이동 및 맨 위로 함수 추가

--- src/features/place/hooks/index.ts
+++ src/features/place/hooks/index.ts
@@ -1,6 +1,5 @@
 export * from './usePlaceToast';
 export * from './useSearchBar';
-export * from './useScrollRestoration';
 export * from './useBottomSheetSnapManagement';
 export * from './useGlobalFocusHandler';
 export * from './usePlaceSearchParams';
