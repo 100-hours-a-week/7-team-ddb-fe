e4cf584dbf2bd068a4e6b67cf2313b78c884f67a
suzykang <pc_suzi@daum.net>
2025-07-08 19:15:10 +0900

    Chore/#194 CDN 주소 env 파일로 관리되도록 변경

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,23 @@
 import type { NextConfig } from 'next';
 
+const ENV = process.env.ENV_LABEL;
+const cdnHost = ENV === 'prod' ? 'cdn.dolpin.site' : 'cdn.dev.dolpin.site';
+
 const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
     NEXT_PUBLIC_KAKAO_MAP_API_KEY: process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY,
+    NEXT_PUBLIC_CDN_HOST: cdnHost,
   },
   images: {
-    domains: ['cdn.dev.dolpin.site', 'cdn.dolpin.site'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: cdnHost,
+        pathname: '/**',
+        port: '',
+      },
+    ],
   },
 };
 
